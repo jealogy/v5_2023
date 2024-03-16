edefebee2504a40f2947ade35b9c0984e899edef
Jea Perez <hi@jeaperez.com>
2024-03-16 06:06:40 -0700

    Add <?php wp_head(); ?>

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/typicons/2.0.8/typicons.min.css">
 <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'><link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>">
-
+<?php wp_head(); ?>
 </head>
 <body>
     <div id="app">
