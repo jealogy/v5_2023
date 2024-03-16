96b464b60bd7e5da95cf0c21740604921c121096
Jea Perez <hi@jeaperez.com>
2024-03-16 04:49:07 -0700

    Create searchform.php

---
+++ searchform.php
@@ -0,0 +1,3 @@
+<form role="search" method="get" id="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+ <input type="text" placeholder="Search" class="input-search" name="s">
+ </form>
