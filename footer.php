9ba6ecffde6ee088100a540a049e5f7e885e5cff
Jea Perez <hi@jeaperez.com>
2024-03-16 04:44:53 -0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,31 @@
+ <div id="search"><a href="#" class="btn-menu" id='btn-search'><i class="fa fa-search fa-fw fa-2x"></i></a> </div>
+ <div id="dim-screen" class="dim-screen hidden">
+      <div id="wrapperWindow" class="wrapper-window">
+
+<div id="menu-search" class="window menu-search hidden">
+            <ul class="search-main">
+              <li>
+				  <?php get_search_form(); ?>
+              </li>
+            </ul>
+          </div>
+          </div>
+          </div>
+ </div>
+</div>
+<footer>
+  <div class="container">
+    <div class="nine columns">
+		<p align="center">Version 5.0 | Designed and coded by Jea Perez <br />© Jea Perez, 2016-2024 unless stated otherwise. See <a href="https://jeaperez.com/privacy-policy-terms-conditions/">Privacy Policy, Terms & Conditions</a></p>
+    </div>
+    <div class="three columns"> </div>
+  </div>
+</footer>
+</div>
+
+<!-- partial -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.10/vue.min.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script><script  src="<?php echo get_template_directory_uri(); ?>/script.js"></script>
+<?php wp_footer(); ?>
+</body>
+</html>
