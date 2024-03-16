3cb20fa08b56f55b33243aa43a36407b9a4f2e58
Jea Perez <hi@jeaperez.com>
2024-03-16 04:44:20 -0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="light">
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Jea Perez</title>
+   <link rel="icon" type="image/x-icon" href="http://jeaperez.com/favicon.ico">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/typicons/2.0.8/typicons.min.css">
+<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'><link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>">
+
+</head>
+<body>
+    <div id="app">
+<header class="cd-header">
+		<div class="header-wrapper">
+			<div class="logo-wrap"><div class="theme-switch-wrapper">
+    <label class="theme-switch" for="checkbox">
+        <input type="checkbox" id="checkbox" />
+        <div class="slider round"></div>
+  </label>
+</div>
+			</div>
+			<div class="nav-but-wrap">
+				<div class="menu-icon hover-target">
+					<span class="menu-icon__line menu-icon__line-left"></span>
+					<span class="menu-icon__line"></span>
+					<span class="menu-icon__line menu-icon__line-right"></span>
+				
+				</div>					
+			</div>					
+		</div>				
+	</header>
+
+	<div class="nav">
+		<div class="nav__content">
+			<ul class="nav__list">
+				<li class="nav__list-item"><a href="http://jeaperez.com" class="hover-target">home</a></li>
+				<li class="nav__list-item"><a href="http://jeaperez.com/about-2" class="hover-target">about</a></li>
+				<li class="nav__list-item"><a href="http://jeaperez.com/manifesto" class="hover-target">manifesto</a></li>
+				<li class="nav__list-item"><a href="http://jeaperez.com/contact" class="hover-target">contact</a></li>
+				<li class="nav__list-item"><a href="https://jea.pics/index.html" target="_blank" class="hover-target">pics</a></li>
+			</ul>
+		</div>
+	</div>				
+
+<div class="menu__toggler"><span></span></div>
+
+<section class="title container">
+  <div class="row">
+    <div class="col-md-12">
+        <span class="typcn typcn-heart-outline icon heading"></span>
+        <div class="seperator"></div>
+      <h1>JEA PEREZ</h1>
+   <div class="welcome">Navigating life in my 20s with the core philosophy of Kaizen</div>
+    </div>
+  </div>
+</section>
+        
+<!-- Start Blog Layout -->
+<div class="container">
+    
+  <div class="row">
